8e5b922631a9073181a901b249242ebfaa28f04e
aanchlia <aanchlia@gmail.com>
2014-05-29 13:27:39 -0500

    Add new class for parsing

--- application/controllers/setup.php
+++ application/controllers/setup.php
@@ -7,14 +7,14 @@ class Parse extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
-        //$this->load->library('doctrine');
         $this->em = $this->doctrine->em;
+        $this->load->library('CommonfloorParse');
     }
 
     public function index(){
-        $edb = new EMAIL_TO_DB();
-        $edb->connect('mail.google.com', '', 'aanchlia@gmail.com', '');
-        $edb->do_action();
+
+        $this->commonfloorparse->connect('mail.google.com', 'aanchlia@gmail.com', '');
+        $this->commonfloorparse->do_action();
         $data = $this->em->getRepository('Model\OwnerInfo')->findOneBy(array('id' => 2));
         print_r($data);
 
---
+++ application/libraries/CommonfloorParse.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 5/29/14
+ * Time: 12:42 PM
+ */ 
